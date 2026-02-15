498ddc96d7bd31419fe98ea43c3e2b74ded36fa5
ngodahieu <ngodahieupbc@gmail.com>
2026-02-15 20:46:26 +0700

    Update script.js

--- script.js
+++ script.js
@@ -21,3 +21,28 @@ const sidebar = document.getElementById("sidebar");
 toggleBtn.addEventListener("click", () => {
     sidebar.classList.toggle("collapsed");
 });
+// Notification toggle
+const notifyBtn = document.getElementById("notifyBtn");
+const notifyMenu = document.getElementById("notifyMenu");
+
+notifyBtn.addEventListener("click", (e) => {
+    e.stopPropagation();
+    notifyMenu.classList.toggle("show");
+    avatarMenu.classList.remove("show");
+});
+
+// Avatar toggle
+const avatarBtn = document.getElementById("avatarBtn");
+const avatarMenu = document.getElementById("avatarMenu");
+
+avatarBtn.addEventListener("click", (e) => {
+    e.stopPropagation();
+    avatarMenu.classList.toggle("show");
+    notifyMenu.classList.remove("show");
+});
+
+// Click ngoài để đóng
+document.addEventListener("click", () => {
+    notifyMenu.classList.remove("show");
+    avatarMenu.classList.remove("show");
+});
